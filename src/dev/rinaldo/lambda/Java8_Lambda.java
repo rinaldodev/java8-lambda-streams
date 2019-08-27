0f5750b9875a2ff481465a7311d98bb074e39210
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2019-08-27 20:30:55 -0300

    Tipos de Lambda

---
+++ src/dev/rinaldo/lambda/Java8_Lambda.java
@@ -0,0 +1,39 @@
+package dev.rinaldo.lambda;
+
+import java.util.stream.IntStream;
+
+/**
+ * JAVA 8 - Funções Lambda - Tipos
+ * 
+ * @author RinaldoDev
+ */
+public class Java8_Lambda {
+
+  public static void main(String[] args) {
+
+    // Parênteses
+    Runnable runnable = () -> System.out.println("Se inscreva no canal!");
+    
+    IntStream.range(0, 5)
+      .filter((int n) -> n % 2 == 0)
+      .reduce((n1, n2) -> n1 + n2)
+      .ifPresent(System.out::println);
+      
+    // Chaves
+    IntStream.range(0, 5)
+      .filter((int n) -> {
+        System.out.println("Se inscreva no canal!");
+        return n % 2 == 0;
+      })
+      .forEach(System.out::println);
+    
+  }
+
+}
+
+//YouTube:  RinaldoDev
+//Twitter:  @rinaldodev
+//Facebook: RinaldoDev
+//GitHub:   rinaldodev
+//LinkedIn: rinaldodev
+//Site:     rinaldo.dev
